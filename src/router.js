e4db45aca56f91dadc8b4f747e8ece57db11c7fe
LiuXingJun <475806058@qq.com>
2019-01-11 20:05:32 +0800

    eslint

--- src/router.js
+++ src/router.js
@@ -1,20 +1,21 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+// import Vue from "vue";
+import Router from "vue-router";
+// import Home from "./views/Home.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
-    {path: '/',name: 'home',component: Home},
     {
-      path: '/furniture',
-      name: 'furniture',
-       // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "furniture" */ './views/furniture.vue')
+      path: "/",
+      name: "home",
+      component: () => import(/* webpackChunkName: "home" */ "./views/Home.vue")
+    },
+    {
+      path: "/furniture",
+      name: "furniture",
+      component: () =>
+        import(/* webpackChunkName: "furniture" */ "./views/furniture.vue")
     }
-
   ]
-})
+});
---
+++ vue.config.js
@@ -0,0 +1,21 @@
+
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  configureWebpack: {
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            compress: {
+              warnings: false,
+              drop_console: true, //console
+              drop_debugger: false,
+              pure_funcs: ["console.log"] //移除console
+            }
+          }
+        })
+      ]
+    }
+  }
+};
