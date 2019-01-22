eddc84c8e71a7ee5f25fbb165aba9768141273e4
liuxingjun <475806058@qq.com>
2019-01-23 02:42:44 +0800

    assetsDir

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   // publicPath:'./',
-  assetsDir:"./bh3",
+  assetsDir:"./BH3",
   configureWebpack: {
     optimization: {
       minimizer: [
